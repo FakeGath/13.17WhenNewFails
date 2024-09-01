98e3611bfe91743cf6e9bfdd2286adcd32d7affe
FakeGath <givanalvaradoath@gmail.com>
2024-09-01 15:55:34 -0600

    Solution for try and catch

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main(){
+
+    
+/*
+    
+    
+    for (size_t i = 0; i < 1000; i++)
+    {
+        try
+    {
+        int * data = new int[100000000000];
+       std::cout<<"Program ending well"<<std::endl;
+    }
+    catch(const std::exception& ex)
+    {
+        std::cout<<"Exception: "<<ex.what()<<std::endl;
+    }
+    
+    }*/
+
+
+    for (size_t i = 0; i < 1000; i++)
+    {
+     
+        int * data = new int[100000000000];
+      
+    }
+    
+    
+    
+    
+return 0;
+
+}
